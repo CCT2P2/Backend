4e3225b18b9c8b9cba21a6546e3b3433a00708a3
lilleole <burhennemalte1@gmail.com>
2025-04-10 11:25:31 +0200

    fixed db

--- gnufv2/obj/Debug/net9.0/gnufv2.AssemblyInfo.cs
+++ gnufv2/obj/Debug/net9.0/gnufv2.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f099ff575206653c540d92c72761934eb120a80")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ed78eb604f6e1bcd9145cf4d6660941dc05c3b")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e871878d85c850b22179c3c0259c5229c9a91ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
