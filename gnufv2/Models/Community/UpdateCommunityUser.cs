6f34b646f0511c1aa08433ba43385d2b59c4293c
Sebastian Lindau-Skands <sebastian.lindauskands@proton.me>
2025-04-19 16:51:13 +0200

    fixed several issues in communitycontroller relating to update

--- gnufv2/Models/Community/UpdateCommunityUser.cs
+++ gnufv2/Models/Community/UpdateCommunityUser.cs
@@ -2,7 +2,7 @@ namespace Gnuf.Models.Community;
 
 public class UpdateCommunityUserRequest
 {
+    public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string? Img { get; set; }
+    public string Img_path { get; set; } = string.Empty;
 }
-
--- gnufv2/obj/Debug/net9.0/gnufv2.AssemblyInfo.cs
+++ gnufv2/obj/Debug/net9.0/gnufv2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31dcd7573c4fb02c1b302d52080a28ddb4d63854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fb01f1b7a307733c1d5ef75df50259511402da")]
 [assembly: System.Reflection.AssemblyProductAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gnufv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
