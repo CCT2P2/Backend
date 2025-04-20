225012a028a064ee56366d54e92fe31ebf9b4754
lilleole <burhennemalte1@gmail.com>
2025-04-20 15:28:34 +0200

    did something

--- gnufv2/Models/Posts/getPostsById.cs
+++ gnufv2/Models/Posts/getPostsById.cs
@@ -1,5 +1,9 @@
 ﻿namespace Gnuf.Models.Posts;
 
+public class PostByIdQueryParameters
+{
+    public string? ids { get; set; }
+}
 public class GetPostsByIdResponse
 {
     public List<PostStructure> posts { get; set; } = new();
